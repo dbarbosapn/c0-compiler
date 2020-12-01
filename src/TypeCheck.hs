66b2cd6c6e95c9cee22e941596274d2cfb585bc2
Énio Sousa <eniofcup@hotmail.com>
2020-12-01 16:05:24 +0000

    Simplifiquei as funções e adicionei multiple Statements chck

--- src/TypeCheck.hs
+++ src/TypeCheck.hs
@@ -60,87 +60,127 @@ checkExpr table (BinaryOperation (RelationalOperation something)) =
     else
        error "Type error in RelationalOperation. Invalid type"
 
-
-
 -- Statements
-checkStm :: SymTable -> Statement -> Bool
-checkStm table (Simple something) =
-  True
-  where
-    table1 = checkSimpleStm table something
+checkStm :: SymTable -> Statement -> SymTable
+-- Simple
+checkStm table (Simple something) = checkSimpleStm table something
 
-checkStm table (IfStatement e1 stm1) =
-  let t1 = checkExpr table e1
-  in
-    if t1 == TypeBool
-    then
-      checkStm table stm1
-    else
-       error "type error: condition must be bool"
+--Ifs
+checkStm table (IfStatement expr stm) =
+  case checkExpr table expr of
+    TypeBool -> checkStm table stm
+    _ -> error "IfStatement: Condition must be bool"
 
-checkStm table (IfElseStatement e1 stm1 stm2) =
-  let t1 = checkStm table (IfStatement e1 stm1)
-      t2 = checkStm table stm2
-  in
-    if t1 && t2
-    then
-      True
-    else
-      error "Something wrong with the ifelsestatement"
+checkStm table (IfElseStatement expr stm1 stm2) =
+  checkStm (checkStm table (IfStatement expr stm1)) stm2
 
-checkStm table (WhileStatement e1 stm1) =
-  let t1 = checkExpr table e1
-  in
-    if t1 == TypeBool
-    then
-      checkStm table stm1
-    else
-       error "type error: condition must be bool"
+-- While
+checkStm table (WhileStatement expr stm) =
+  case checkExpr table expr of
+    TypeBool -> checkStm table stm
+    _ -> error "WhileStatement: Condition must be bool"
 
-checkStm table (ForStatement (something1, e1, something2) stm) =
-  let t1 = checkExpr table e1
-      table' = case something1 of
-                 Nothing -> table
-                 Just some1 -> checkSimpleStm table some1
-      table'' = case something2 of
-                  Nothing -> table'
-                  Just some2 -> checkSimpleStm table' some2
-  in
-    if t1 == TypeBool
-    then
-      checkStm table'' stm
-    else
-      error "Type error: condition must be bool"
+-- For
+checkStm table (ForStatement (stm1, expr, stm2) stm3) =
+  case checkExpr table expr of
+    TypeBool -> let table' = case stm1 of
+                               Nothing -> table
+                               Just something -> checkSimpleStm table something
+                    table'' = case stm2 of
+                                Nothing -> table'
+                                Just something -> checkSimpleStm table' something
+                in
+                  table''
+    _-> error "ForStatement: Condition must be bool"
 
--- Complete: Look for the return type of function
-checkStm table (ReturnStatement something) =
-  let t1 = case something of
-             Just e1 -> checkExpr table e1
-             Nothing -> TypeVoid
-  in
-    True
+-- Multiple statements
+checkStm table (MultipleStatements [x]) =
+  checkStm table x
+checkStm table (MultipleStatements (x:xs)) =
+  checkStm (checkStm table x) (MultipleStatements xs)
 
-
+-- Simple Checks. Contains Variable Declaration (Important)
 checkSimpleStm :: SymTable -> Simple -> SymTable
-checkSimpleStm table (AssignOperation (Assign id e1)) =
+checkSimpleStm table (AssignOperation (Assign id expr)) =
   case Map.lookup id table of
     Nothing -> error "undeclared var"
-    Just t1 -> let t2 = checkExpr table e1
-               in
-                 if t1 == t2
-                 then
-                   table
-                 else
-                   error "type error in assignment"
+    Just t -> let t1 = checkExpr table expr
+              in
+                if t == t1 then
+                  table
+                else
+                  error "type error in assignment"
 
-checkSimpleStm table (VariableDeclaration t1 id e1) =
-  case e1 of
-    Nothing -> Map.insert id t1 table
-    Just e1 -> checkSimpleStm (Map.insert id t1 table) (AssignOperation (Assign id e1))
+checkSimpleStm table (VariableDeclaration t id expr) =
+  case Map.lookup id table of
+    Just t1 -> error "Variable already declared"
+    Nothing -> case expr of
+                 Nothing -> Map.insert id t table
+                 Just expr1 -> checkSimpleStm (Map.insert id t table) (AssignOperation (Assign id expr1))
 
--- In c0 you can just have an expression by itself, BUT you sill need to check
--- if its well constructed.
--- Improve this "function" looks weird
-checkSimpleStm table (Expression e1) =
-  case checkExpr table e1 of
+-- In c0 we can have a expression alone, but it still needs to be checked
+checkSimpleStm table (Expression expr) =
+  case checkExpr table expr of
     _ -> table
+
+-- -- Statements
+-- checkStm :: SymTable -> Statement -> Maybe SymTable
+-- checkStm table (Simple something) =
+--   case (checkSimpleStm table something) of
+--     Nothing -> Nothing
+--     Just table' -> Just table'
+
+-- checkStm table (IfStatement e1 stm1) =
+--   case (checkExpr table e1) of
+--     TypeBool -> checkStm table stm1
+--     _ ->  error "type error: condition must be bool"
+
+-- checkStm table (IfElseStatement e1 stm1 stm2) =
+--   case (checkStm table (IfStatement e1 stm1)) of
+--     Just table' -> (checkStm table' stm2)
+--     Nothing -> Nothing
+
+-- checkStm table (WhileStatement e1 stm1) =
+--   case (checkExpr table e1) of
+--     TypeBool -> checkStm table stm1
+--     _ -> error "type error: condition must be bool"
+
+-- checkStm table (ForStatement (something1, e1, something2) stm) =
+--   let t1 = checkExpr table e1
+--       table' = case something1 of
+--                  Nothing -> table
+--                  Just some1 -> checkSimpleStm table some1
+--       table'' = case something2 of
+--                   Nothing -> table'
+--                   Just some2 -> checkSimpleStm table' some2
+--   in
+--     if t1 == TypeBool
+--     then
+--       checkStm table'' stm
+--     else
+--       error "Type error: condition must be bool"
+
+
+-- checkSimpleStm :: SymTable -> Simple -> Maybe SymTable
+-- checkSimpleStm table (AssignOperation (Assign id e1)) =
+--   case Map.lookup id table of
+--     Nothing -> error "undeclared var"
+--     Just t1 -> let t2 = checkExpr table e1
+--                in
+--                  if t1 == t2
+--                  then
+--                    table
+--                  else
+--                    error "type error in assignment"
+
+-- checkSimpleStm table (VariableDeclaration t1 id e1) =
+--   case e1 of
+--     Nothing -> Map.insert id t1 table
+--     Just e1 -> checkSimpleStm (Map.insert id t1 table) (AssignOperation (Assign id e1))
+
+-- -- In c0 you can just have an expression by itself, BUT you sill need to check
+-- -- if its well constructed.
+-- -- Improve this "function" looks weird
+-- checkSimpleStm table (Expression e1) =
+--   case checkExpr table e1 of
+--     _ -> table
